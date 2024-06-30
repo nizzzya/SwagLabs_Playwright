d7a8c7d7083dbcda271709c61f880b3a69a37df8
NizZya <test@email.com>
2024-06-30 23:59:26 +0200

    Add best selectors

--- src/pages/LoginPage.ts
+++ src/pages/LoginPage.ts
@@ -12,8 +12,10 @@ export default class LoginPage {
     }
 
     async login(username: string, password: string) {
-        await this.page.fill('input[name="user-name"]', username);
-        await this.page.fill('input[name="password"]', password);
+        await this.page.getByPlaceholder('Username').fill(username);
+        await this.page.getByRole('textbox', { name: 'Password' }).fill(password);
+        // await this.page.fill('input[name="user-name"]', username);
+        // await this.page.fill('input[name="password"]', password);
         await this.page.getByRole('button', { name: 'LOGIN' }).click();
     }
 }
